4ed20d2c0b210fa2ee22b538b488d9d096655546
BrianEsteban <bebarretoc@unal.edu.co>
2017-03-05 00:02:11 -0500

    Update Main.java

--- src/Soccer/Main.java
+++ src/Soccer/Main.java
@@ -10,5 +10,31 @@ package Soccer;
  * @author Grimma
  */
 public class Main {
+    public static void Main (String[] args){
+        
+                Soccer fut = new Soccer();
+		        fut.jugar();
+                fut.avanzar();
+                fut.avanzar();
+                fut.girarDerecha();
+                fut.avanzar();
+                fut.girarIzquierda();
+                fut.regate();
+                fut.avanzar();
+                fut.avanzar();
+                fut.girarDerecha();
+                fut.avanzar();
+                fut.girarIzquierda();
+                fut.regate();
+                fut.avanzar();
+                fut.avanzar();
+                fut.regate();
+                fut.girarIzquierda();
+                fut.avanzar();      
+                fut.girarDerecha();
+                fut.avanzar();
+                fut.avanzar();
+                fut.gol();
+	}
     
 }
